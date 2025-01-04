f5e16f9dee35d727c718b07f99ba49eef8b13e12
Kwadjo Boadi-Mantey <kwadjo.boadi-mantey@amalitech.com>
2025-01-04 11:48:32 +0000

    feat: Configure env for unit testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: "jsdom",
-        setupFiles: ["./setupTest.ts"]
+        setupFiles: ["./setupTest.ts"],
+        include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     },
 });
