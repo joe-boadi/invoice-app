5b74c05c8f79f0b68f7e068a625f38d3bcf39573
Kwadjo Boadi-Mantey <kwadjo.boadi-mantey@amalitech.com>
2025-01-13 12:45:23 +0000

    feat: setup interfdaces for datatypes and component

---
+++ src/auth/interface.ts
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react'
+
+export interface User {
+    email: string;
+    name: string;
+    password: string;
+}
+
+export interface ProtectedRouteProps {
+  children: ReactNode;
+}
+export interface AuthServiceInterface {
+    saveToken: (token: string) => void;
+    getToken: () => string | null;
+    removeToken: () => void;
+    isAuthenticated: () => boolean;
+}
