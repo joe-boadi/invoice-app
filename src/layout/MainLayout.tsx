bb21a03aede58bc759fbf6821939558ee3ac80f2
Kwadjo Boadi-Mantey <kwadjo.boadi-mantey@amalitech.com>
2025-01-13 19:35:28 +0000

    Route user to main layout after login

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,23 +1,30 @@
 // rrd imports
-import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { Link, Outlet, ScrollRestoration } from 'react-router-dom';
 
 // components
 import Navbar from './Navbar';
 import OffCanvas from '../components/offCanvas/OffCanvas';
 import Modal from '../components/modal/Modal';
+// import Homepage from './Homapage';
+// import { useState } from 'react';
 
 const MainLayout = () => {
+	// const [isLoggedin, setIsLoggedin] = useState(false);
+	
 	return (
-		<div className='layout'>
-			<Navbar />
-			<main>
-				<Outlet />
-				<OffCanvas />
-				<Modal />
-			</main>
-			<ScrollRestoration />
-		</div>
-	);
+    // layout component goes here...
+    <div className="layout">
+      <Link to="/app">
+        <Navbar />
+        <main>
+          <Outlet />
+          <OffCanvas />
+          <Modal />
+        </main>
+        <ScrollRestoration />
+      </Link>
+    </div>
+  );
 };
 
 export default MainLayout;
